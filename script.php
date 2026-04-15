70b0ec515189c0dce48763a56c4f10562bc039a3
LuisMS05 <a.luismejias@seoane.edu.pe>
2026-04-15 10:58:17 -0500

    Optimizacion de la funcion sumar

--- script.php
+++ script.php
@@ -1,5 +1,19 @@
 <?php 
+//Aplicando tipado fuerte
+declare(strict_types=1);
 //Funcion suma
 function sumar($a,$b){
-    return $a+$b;
+
+
+    //Añadiendo excepcion para el manejo de errores
+    try{
+        if(!is_numeric($a) || !is_numeric($b)){
+            throw new Exception('uno o mas valores son invalidos');
+        }
+        echo 'El resultado de la suma es: '.$a+$b;
+
+    }catch(Exception $ex){
+        echo 'Se ha producido una excpecion '.$ex->getMessage();
+    }
+    
 }
